15dde4b07374c878beee2a0ffb93be01173495c8
Ayesha Nizam <ayeshanizam39@gmail.com>
2025-11-04 22:18:25 +0500

    Add

--- src/Components/WhatsAppButton.js
+++ src/Components/WhatsAppButton.js
@@ -3,7 +3,7 @@ import React from 'react';
 const WhatsAppButton = () => {
   return (
     <a 
-      href="https://wa.me/03316233334?text=Hello!%20Ca%I%20learn%20more%20about%20your%20business?" // Replace with your WhatsApp number
+      href="https://wa.me/03316233334?text=Hello!%20Can%20I%20learn%20more%20about%20your%20business?" // Replace with your WhatsApp number
       target="_blank" 
       rel="noopener noreferrer"
       className="fixed bottom-4 right-4 bg-green-500 text-white p-4 rounded-full shadow-lg hover:bg-green-600 animate-pulse-slow"
