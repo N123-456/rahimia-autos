89858d0f991aaac47817c6359fc7c2e583ffc2d5
Ayesha Nizam <ayeshanizam39@gmail.com>
2025-11-04 20:35:38 +0500

    Add

--- src/pages/Intro.js
+++ src/pages/Intro.js
@@ -1,62 +1,51 @@
-import React from 'react';
+import React from "react";
+import "flowbite";
 
 const Intro = () => {
-    return (
-        <>
-<section className='mb-3'>
-            <div id="default-carousel" className="relative w-full" data-carousel="slide">
+  return (
+    <section className="mb-3">
+      <div id="default-carousel" className="relative w-full" data-carousel="slide">
+        <div className="relative h-56 overflow-hidden rounded-lg md:h-96">
 
-                <div className="relative h-56 overflow-hidden rounded-lg md:h-96">
+          <div className="duration-700 ease-in-out" data-carousel-item>
+            <img
+              src="/images/bumpers.png"
+              className="w-full h-full object-cover absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2"
+              alt="Bumpers"
+            />
+          </div>
 
-                    <div className="hidden duration-700 ease-in-out" data-carousel-item>
-                        <img  src="images/bumpers.png" className="absolute block w-full -translate-x-1/2 -translate-y-1/2 top-1/2 left-1/2" alt="..." />
-                    </div>
+          <div className="hidden duration-700 ease-in-out" data-carousel-item>
+            <img
+              src="https://as1.ftcdn.net/v2/jpg/02/68/08/36/1000_F_268083642_E6iGmU3rBXqm9lyojdw5VNcuXdbrZv0e.jpg"
+              className="w-full h-full object-cover absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2"
+              alt="Car"
+            />
+          </div>
 
-                    <div className="hidden duration-700 ease-in-out" data-carousel-item>
-                        <img src="https://as1.ftcdn.net/v2/jpg/02/68/08/36/1000_F_268083642_E6iGmU3rBXqm9lyojdw5VNcuXdbrZv0e.jpg" className="absolute block w-full -translate-x-1/2 -translate-y-1/2 top-1/2 left-1/2" alt="..." />
-                    </div>
+          <div className="hidden duration-700 ease-in-out" data-carousel-item>
+            <img
+              src="/images/headlight.png"
+              className="w-full h-full object-cover absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2"
+              alt="Headlight"
+            />
+          </div>
+        </div>
 
-                    <div className="hidden duration-700 ease-in-out" data-carousel-item>
-                        <img src="images/headlight.jpg" className="absolute block w-full -translate-x-1/2 -translate-y-1/2 top-1/2 left-1/2" alt="..." />
-                    </div>
-
-                    <div className="hidden duration-700 ease-in-out" data-carousel-item>
-                        <img src="https://as1.ftcdn.net/v2/jpg/14/77/14/92/1000_F_1477149246_bTOB1kdgp0NIMZ8UsiBiNCllHawsz1yo.jpg" className="absolute block w-full -translate-x-1/2 -translate-y-1/2 top-1/2 left-1/2" alt="..." />
-                    </div>
-
-                    <div className="hidden duration-700 ease-in-out" data-carousel-item>
-                        <img src="https://as2.ftcdn.net/v2/jpg/01/70/07/97/1000_F_170079736_3j492cNVeszAEFt2aAgMgpXR1f6VbFrV.jpg" className="absolute block w-full -translate-x-1/2 -translate-y-1/2 top-1/2 left-1/2" alt="..." />
-                    </div>
-                </div>
-
-                <div className="absolute z-30 flex -translate-x-1/2 bottom-5 left-1/2 space-x-3 rtl:space-x-reverse">
-                    <button type="button" className="w-3 h-3 rounded-full" aria-current="true" aria-label="Slide 1" data-carousel-slide-to="0"></button>
-                    <button type="button" className="w-3 h-3 rounded-full" aria-current="false" aria-label="Slide 2" data-carousel-slide-to="1"></button>
-                    <button type="button" className="w-3 h-3 rounded-full" aria-current="false" aria-label="Slide 3" data-carousel-slide-to="2"></button>
-                    <button type="button" className="w-3 h-3 rounded-full" aria-current="false" aria-label="Slide 4" data-carousel-slide-to="3"></button>
-                    <button type="button" className="w-3 h-3 rounded-full" aria-current="false" aria-label="Slide 5" data-carousel-slide-to="4"></button>
-                </div>
-
-                <button type="button" className="absolute top-0 start-0 z-30 flex items-center justify-center h-full px-4 cursor-pointer group focus:outline-none" data-carousel-prev>
-                    <span className="inline-flex items-center justify-center w-10 h-10 rounded-full bg-white/30 dark:bg-gray-800/30 group-hover:bg-white/50 dark:group-hover:bg-gray-800/60 group-focus:ring-4 group-focus:ring-white dark:group-focus:ring-gray-800/70 group-focus:outline-none">
-                        <svg className="w-4 h-4 text-white dark:text-gray-800 rtl:rotate-180" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 6 10">
-                            <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M5 1 1 5l4 4" />
-                        </svg>
-                        <span className="sr-only">Previous</span>
-                    </span>
-                </button>
-                <button type="button" className="absolute top-0 end-0 z-30 flex items-center justify-center h-full px-4 cursor-pointer group focus:outline-none" data-carousel-next>
-                    <span className="inline-flex items-center justify-center w-10 h-10 rounded-full bg-white/30 dark:bg-gray-800/30 group-hover:bg-white/50 dark:group-hover:bg-gray-800/60 group-focus:ring-4 group-focus:ring-white dark:group-focus:ring-gray-800/70 group-focus:outline-none">
-                        <svg className="w-4 h-4 text-white dark:text-gray-800 rtl:rotate-180" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 6 10">
-                            <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="m1 9 4-4-4-4" />
-                        </svg>
-                        <span className="sr-only">Next</span>
-                    </span>
-                </button>
-            </div>
-</section>
-        </>
-    );
+        {/* Controls */}
+        <button type="button" className="absolute top-0 start-0 z-30 flex items-center justify-center h-full px-4 cursor-pointer group" data-carousel-prev>
+          <span className="inline-flex items-center justify-center w-10 h-10 bg-white/30 rounded-full group-hover:bg-white/50">
+            <svg className="w-4 h-4 text-white" fill="none" viewBox="0 0 6 10"><path stroke="currentColor" strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M5 1 1 5l4 4" /></svg>
+          </span>
+        </button>
+        <button type="button" className="absolute top-0 end-0 z-30 flex items-center justify-center h-full px-4 cursor-pointer group" data-carousel-next>
+          <span className="inline-flex items-center justify-center w-10 h-10 bg-white/30 rounded-full group-hover:bg-white/50">
+            <svg className="w-4 h-4 text-white" fill="none" viewBox="0 0 6 10"><path stroke="currentColor" strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="m1 9 4-4-4-4" /></svg>
+          </span>
+        </button>
+      </div>
+    </section>
+  );
 };
 
 export default Intro;
